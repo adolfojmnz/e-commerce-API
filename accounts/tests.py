64d7c009e333b89cd838014c1fc449421aa19482
Adolfo Jimenez <adolfoj@protonmail.com>
2023-04-11 11:04:32 -0400

    Improve redability

--- accounts/tests.py
+++ accounts/tests.py
@@ -5,7 +5,7 @@ from accounts.models import User
 
 class CreateUserMixin:
 
-    def create_test_user(self):
+    def create_user(self):
         user = User.objects.create(
             username = 'test-user',
             email = 'user@testsuit.com',
@@ -17,10 +17,19 @@ class CreateUserMixin:
 class TestUser(CreateUserMixin, TestCase):
 
     def setUp(self):
-        self.user = self.create_test_user()
+        self.user = self.create_user()
         self.user.save()
 
     def test_user(self):
-        self.assertEqual(User.objects.filter(username='test-user').exists(), True)
-        self.assertEqual(User.objects.get(username='test-user').email, self.user.email)
-        self.assertEqual(User.objects.get(username='test-user').password, self.user.password)
+        self.assertEqual(
+            User.objects.filter(username='test-user').exists(),
+            True,
+        )
+        self.assertEqual(
+            User.objects.get(username='test-user').email,
+            self.user.email,
+        )
+        self.assertEqual(
+            User.objects.get(username='test-user').password,
+            self.user.password,
+        )
