e16427415ea3a6d326836bac10693a964aa2ffe8
Adolfo Jimenez <adolfoj@protonmail.com>
2023-04-12 11:19:28 -0400

    Rename field `bio` to `about` in `User` model

--- accounts/models.py
+++ accounts/models.py
@@ -3,17 +3,17 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    '''
+    """
     default fields:
         username, first_name, last_name, email, password, group, user_permissions,
         is_staff, is_active, is_superuser, last_login, date_joined
-    '''
+    """
     # for further information refer to https://docs.djangoproject.com/en/4.1/ref/contrib/auth/
 
     # custom fields
     avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
     birthdate = models.DateField(auto_now=True)
-    bio = models.CharField(default='', max_length=1000)
+    about = models.CharField(default='', max_length=512)
 
     def __str__(self):
         return self.username
