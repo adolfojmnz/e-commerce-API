f91e052a710c9cb5603ccec7af9e96dfdda1db82
Adolfo Jimenez <adolfoj@protonmail.com>
2023-04-12 08:21:33 -0400

    Add tests for `WatchPhoto` model

--- tests/photos/tests.py
+++ tests/photos/tests.py
@@ -1,3 +1,39 @@
 from django.test import TestCase
 
-# Create your tests here.
+from photos.models import WatchPhoto
+
+from tests.watches.tests import SetUpMixin as SetUpWatchMixin
+
+import os
+
+
+class SetUpMixin(SetUpWatchMixin):
+
+    WATCH_PHOTO_DATA = {
+        'main': True,
+        'file': os.path.join(
+            os.path.abspath('.'), 'tests/photos/photo.jpeg'
+        )
+    }
+
+    def upload_watch_photo(self, watch):
+        self.WATCH_PHOTO_DATA['watch'] = watch
+        photo = WatchPhoto.objects.create(**self.WATCH_PHOTO_DATA)
+        photo.save()
+        return photo
+
+
+class TestWatchPhoto(SetUpMixin, TestCase):
+
+    def setUp(self):
+        vendor = self.create_user()
+        collection = self.create_collection(vendor)
+        watch = self.create_watch(collection)
+        self.watch_photo = self.upload_watch_photo(watch)
+
+    def test_watch_photo(self):
+        retrieved_photo = WatchPhoto.objects.get(pk=self.watch_photo.pk)
+        self.assertEqual(self.watch_photo.watch, retrieved_photo.watch)
+        self.assertEqual(self.watch_photo.main, retrieved_photo.main)
+        self.assertEqual(self.watch_photo.file, retrieved_photo.file)
+
