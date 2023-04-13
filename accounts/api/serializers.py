23d66534d4e41e32b9520b0806ec956f188f1f30
Adolfo Jimenez <adolfoj@protonmail.com>
2023-04-13 18:36:08 -0400

    Serialize the `id` field of the `User` model

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -8,6 +8,7 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = [
+            'id',
             'email',
             'username',
             'first_name',
