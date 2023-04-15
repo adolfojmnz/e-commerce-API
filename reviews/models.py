d8288452854f252bb1d518e37d7337d84bcb1809
Adolfo Jimenez <adolfoj@protonmail.com>
2023-04-15 17:27:33 -0400

    Replace `watches` for `products` field

--- reviews/models.py
+++ reviews/models.py
@@ -2,10 +2,12 @@ from django.db import models
 
 
 class Review(models.Model):
-    watch = models.ForeignKey(
-        'watches.Watch', on_delete=models.PROTECT, related_name='reviews',
-    )
-    user = models.ForeignKey('accounts.User', on_delete=models.PROTECT)
+    product = models.ForeignKey('products.Product',
+                                on_delete=models.PROTECT,
+                                related_name='reviews')
+    user = models.ForeignKey('accounts.User',
+                             on_delete=models.PROTECT,
+                             related_name='reviews')
     title = models.CharField(max_length=64)
     text = models.CharField(max_length=512)
     rating = models.IntegerField()
