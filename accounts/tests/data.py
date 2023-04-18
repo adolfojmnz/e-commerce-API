5ade83abec946af27b889274b68f0cad2442fdcd
Adolfo Jimenez <adolfoj@protonmail.com>
2023-04-17 20:03:19 -0400

    Add tests for `api/users` endpoint

---
+++ accounts/tests/data.py
@@ -0,0 +1,40 @@
+users_data_list = [
+    {
+        "username": "john",
+        "password": "q!--qQZR8U4,cZXY",
+        "first_name": "john",
+        "last_name": "doe",
+        "about": "john doe's bio",
+        "email": "johnd@localhost.com",
+        "birthdate": "2000-01-01",
+        "is_active": True,
+        "last_login": "2023-04-11T12:39:52Z",
+        "date_joined": "2023-04-11T12:39:46Z"
+    },
+    {
+        "username": "janedoe",
+        "password": "Qd'/G5Cx5j%Tbc<y",
+        "first_name": "jane",
+        "last_name": "doe",
+        "about": "jane doe's bio",
+        "email": "janed@localhost.com",
+        "birthdate": "2000-02-01",
+        "is_active": True,
+        "last_login": "2023-01-11T12:39:52Z",
+        "date_joined": "2023-01-11T12:39:46Z"
+    },
+]
+
+
+single_user_data = {
+    "username": "richard",
+    "password": "p5%gz^$Zr3/*YUkT",
+    "first_name": "richard",
+    "last_name": "doe",
+    "about": "richard doe's bio",
+    "email": "richard@localhost.com",
+    "birthdate": "2000-01-01",
+    "is_active": True,
+    "last_login": "2023-04-11T12:39:52Z",
+    "date_joined": "2023-04-11T12:39:46Z"
+}
