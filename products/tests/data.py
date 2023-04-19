23d53107bc7a28c81c014dfeddaf7bc8519a822c
Adolfo Jimenez <adolfoj@protonmail.com>
2023-04-19 17:26:18 -0400

    Add products testing data in `tests/data.py`

---
+++ products/tests/data.py
@@ -0,0 +1,46 @@
+products_data_list = [
+    {
+        "name": "Men's Watch Cobalt (Walnut/Blue)",
+        "brand": "Holzkern",
+        "description": """
+            Cobalt is a naturally occurring element that can be found in the
+            ground, the air and also in the bodies of living creatures. Its
+            bonding properties have been used for the coloration of glass and
+            ceramics since antiquity.
+        """,
+        "specifications": {
+            "is waterproof": True,
+            "has date indicator": False,
+            "has cronograph features": False,
+            "color": "Walnut/Blue",
+            "diameter (mm)": 42,
+            "movement": "quartz",
+            "case material": "metal-wood",
+            "bracelet material": "metal-wood",
+            "release date": "2023-04-10"
+        },
+        "price": 499
+    },
+    {
+        "name": "Men's Watch Cobalt (Walnut/Black)",
+        "brand": "Holzkern",
+        "description": """
+            Cobalt is a naturally occurring element that can be found in the
+            ground, the air and also in the bodies of living creatures. Its
+            bonding properties have been used for the coloration of glass and
+            ceramics since antiquity.
+        """,
+        "specifications": {
+            "is waterproof": True,
+            "has date indicator": False,
+            "has cronograph features": False,
+            "color": "Walnut/Black",
+            "diameter (mm)": 42,
+            "movement": "quartz",
+            "case material": "metal-wood",
+            "bracelet material": "metal-wood",
+            "release date": "2023-04-10"
+        },
+        "price": 499
+    },
+]
