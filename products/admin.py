fcaafc84152a524c4b5843814fefb018b853e33f
Adolfo Jimenez <adolfoj@protonmail.com>
2023-04-18 21:54:40 -0400

    Add `products.Inventory` model

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from products.models import Product
+from products.models import Product, Inventory
 
 
 admin.site.register(Product)
+admin.site.register(Inventory)
