47fe58b768fdae1118d041061d6762bee4de8c19
Adolfo Jimenez <adolfoj@protonmail.com>
2023-04-20 15:56:45 -0400

    Remove `products` field from `Category` model

--- categories/models.py
+++ categories/models.py
@@ -4,9 +4,6 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     description = models.CharField(max_length=256)
-    products = models.ForeignKey('products.Product',
-                                 on_delete=models.PROTECT,
-                                 related_name='category')
 
     def __str__(self) -> str:
         return self.name
