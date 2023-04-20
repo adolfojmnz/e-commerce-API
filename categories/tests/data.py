aed6df4383639d318b6f59e236060c7c4f63849e
Adolfo Jimenez <adolfoj@protonmail.com>
2023-04-20 15:57:17 -0400

    Add tests for `Category` model

---
+++ categories/tests/data.py
@@ -0,0 +1,4 @@
+single_category_data = {
+    "name": "Men's Watches",
+    "description": "Classical and modern men's watches",
+}
---
+++ categories/tests/test_category_model.py
@@ -0,0 +1,29 @@
+from django.test import TestCase
+
+from categories.models import Category
+
+from categories.tests.utils import create_category
+
+
+class SetUp(TestCase):
+
+    def setUp(self):
+        self.category = create_category()
+
+
+class TestCategory(SetUp):
+
+    def test_category(self):
+        self.assertEqual(
+            Category.objects.filter(pk=self.category.pk).exists(),
+            True,
+        )
+        self.assertEqual(
+            Category.objects.get(pk=self.category.pk).name,
+            self.category.name,
+        )
+        self.assertEqual(
+            Category.objects.get(pk=self.category.pk).description,
+            self.category.description,
+        )
+
