588dcfd3a2dabbd52924b1817e8516d9a557d617
Adolfo Jimenez <adolfoj@protonmail.com>
2023-04-20 15:33:22 -0400

    Remove `vendor` field from `Iventory` model

--- inventory/models.py
+++ inventory/models.py
@@ -2,18 +2,12 @@ from django.db import models
 
 
 class Inventory(models.Model):
-    vendor = models.ForeignKey('accounts.User',
-                               on_delete=models.PROTECT,
-                               related_name='inventory')
     product = models.ForeignKey('products.Product',
                                 on_delete=models.PROTECT,
                                 related_name='inventory')
-    quantity = models.IntegerField()
+    quantity = models.IntegerField(default=0)
     added_on = models.DateTimeField(auto_now=True)
     updated_on = models.DateField(auto_now=True)
-
-    class Meta:
-        unique_together = ['vendor', 'product']
 
     def __str__(self) -> str:
         return f"""
