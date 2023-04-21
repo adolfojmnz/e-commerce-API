b8f818a7c3366af350891b1fd6364b4d73f22806
Adolfo Jimenez <adolfoj@protonmail.com>
2023-04-21 18:31:07 -0400

    Remove `total` from `Order` model

--- orders/models.py
+++ orders/models.py
@@ -5,10 +5,6 @@ class Order(models.Model):
     user = models.ForeignKey('accounts.User',
                              on_delete=models.PROTECT,
                              related_name='orders')
-    total = models.DecimalField(max_digits=10,
-                                     decimal_places=2,
-                                     blank=True,
-                                     null=True)
     created_on = models.DateTimeField(auto_now=True)
     updated_on = models.DateTimeField(auto_now=True)
 
