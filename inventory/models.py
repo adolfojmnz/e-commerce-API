a758c71f281fe975d565cb52b15c4b95db28698d
Adolfo Jimenez <adolfoj@protonmail.com>
2023-04-22 13:59:59 -0400

    `Inventory.product` is now a OneToOne relationship with `Product`

--- inventory/models.py
+++ inventory/models.py
@@ -2,15 +2,14 @@ from django.db import models
 
 
 class InventoryItem(models.Model):
-    product = models.ForeignKey('products.Product',
+    product = models.OneToOneField('products.Product',
                                 on_delete=models.PROTECT,
-                                related_name='inventory',
-                                unique=True)
+                                related_name='inventory')
     quantity = models.IntegerField(default=0)
     added_on = models.DateTimeField(auto_now=True)
     updated_on = models.DateField(auto_now=True)
 
-    def __str__(self) -> str: 
+    def __str__(self) -> str:
         return f"""
             vendor: {self.product.vendor.username.capitalize()}
             | product: {self.product.name}
