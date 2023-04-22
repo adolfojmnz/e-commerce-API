d300a02ec982e1b4cfda0d19aa6cc4197cebbd9c
Adolfo Jimenez <adolfoj@protonmail.com>
2023-04-22 17:03:05 -0400

    Add `available` and `quantity` on `ProductSerializer`

--- products/api/serializers.py
+++ products/api/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 
 from products.models import Product
 
+from categories.models import Category
+
 
 class ProductSerializer(serializers.ModelSerializer):
 
@@ -9,8 +11,23 @@ class ProductSerializer(serializers.ModelSerializer):
         view_name='user-detail',
         read_only=True,
     )
+    category = serializers.HyperlinkedRelatedField(
+        view_name='category-detail',
+        queryset=Category.objects.all(),
+    )
+    available = serializers.SerializerMethodField()
+    quantity = serializers.SerializerMethodField()
+
+    def get_available(self, obj):
+        return obj.inventory.quantity > 0
+
+    def get_quantity(self, obj):
+        return obj.inventory.quantity
 
     class Meta:
         model = Product
-        fields = '__all__'
+        fields = [
+            'id', 'name', 'brand', 'description', 'specifications',
+            'price', 'vendor', 'category', 'available', 'quantity',
+        ]
 
