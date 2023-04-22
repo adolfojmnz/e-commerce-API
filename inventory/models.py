d8f7d75091378a4086f8f26e5cd3763c74bc9176
Adolfo Jimenez <adolfoj@protonmail.com>
2023-04-22 13:52:18 -0400

    Add `unique=True` constrait on `InventoryItem.product`

--- inventory/models.py
+++ inventory/models.py
@@ -4,15 +4,20 @@ from django.db import models
 class InventoryItem(models.Model):
     product = models.ForeignKey('products.Product',
                                 on_delete=models.PROTECT,
-                                related_name='inventory')
+                                related_name='inventory',
+                                unique=True)
     quantity = models.IntegerField(default=0)
     added_on = models.DateTimeField(auto_now=True)
     updated_on = models.DateField(auto_now=True)
 
     def __str__(self) -> str: 
         return f"""
-            vendor: {self.vendor.username.capitalize()}
+            vendor: {self.product.vendor.username.capitalize()}
             | product: {self.product.name}
             | quantity: {self.quantity}
         """
 
+    class Meta:
+        verbose_name = 'Inventory Item'
+        verbose_name_plural = 'Inventory Items'
+        ordering = ['-added_on']
