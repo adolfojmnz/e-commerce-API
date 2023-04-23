d24cdf254d2a58574b837ab915f762edd702447c
Adolfo Jimenez <adolfoj@protonmail.com>
2023-04-23 08:49:10 -0400

    Add `total` field to `CartSerializer`

--- carts/api/serializers.py
+++ carts/api/serializers.py
@@ -1,3 +1,5 @@
+from django.db.models import Sum
+
 from rest_framework import serializers
 
 from carts.models import Cart, CartItem
@@ -5,9 +7,18 @@ from carts.models import Cart, CartItem
 
 class CartSerializer(serializers.ModelSerializer):
 
+    total = serializers.SerializerMethodField()
+
+    def get_total(self, cart):
+        return cart.cart_items.aggregate(Sum('sub_total'))['sub_total__sum']
+
     class Meta:
         model = Cart
-        fields = '__all__'
+        fields = ['id', 'user', 'total', 'updated_on']
+        extra_kwargs = {
+            'total': {'read_only': True},
+            'updated_on': {'read_only': True},
+        }
 
 
 class CartItemSerializer(serializers.ModelSerializer):
