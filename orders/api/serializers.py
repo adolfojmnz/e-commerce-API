4d4d4f8a23f3a496be96db4333a667d5234c05b3
Adolfo Jimenez <adolfoj@protonmail.com>
2023-04-22 20:06:26 -0400

    Add `total` field to `OrderSerializer`

--- orders/api/serializers.py
+++ orders/api/serializers.py
@@ -1,3 +1,5 @@
+from django.db.models import Sum
+
 from rest_framework import serializers
 
 from orders.models import Order, OrderItem
@@ -5,9 +7,19 @@ from orders.models import Order, OrderItem
 
 class OrderSerializer(serializers.ModelSerializer):
 
+    total = serializers.SerializerMethodField()
+
+    def get_total(self, order):
+        return order.order_items.aggregate(Sum('sub_total'))['sub_total__sum']
+
     class Meta:
         model = Order
-        fields = '__all__'
+        fields = ['id', 'user', 'total', 'created_on', 'updated_on']
+        extra_kwargs = {
+            'total': {'read_only': True},
+            'created_on': {'read_only': True},
+            'updated_on': {'read_only': True},
+        }
 
 
 class OrderItemSerializer(serializers.ModelSerializer):
@@ -15,4 +27,3 @@ class OrderItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = OrderItem
         fields = '__all__'
-
