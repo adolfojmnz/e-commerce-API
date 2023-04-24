465325cd7920c4e7ee6bfc0fe060a4f622c93266
Adolfo Jimenez <adolfoj@protonmail.com>
2023-04-24 10:43:46 -0400

    Rename file + Test users' cart assignment on registration

--- accounts/tests/test_user_list_endpoint.py
+++ accounts/tests/test_user_list_endpoint.py
@@ -5,8 +5,9 @@ from rest_framework import status
 
 from accounts.models import User
 from accounts.api.serializers import UserSerializer
-
-from .data import users_data_list, single_user_data
+from accounts.tests.data import (
+    users_data_list, single_user_data
+)
 
 
 class SetUpMixin(TestCase):
@@ -18,14 +19,13 @@ class SetUpMixin(TestCase):
 class TestUsers(SetUpMixin):
 
     def test_post(self):
-        response = self.client.post(reverse('users'),
-                                    data=single_user_data)
-        serializer = UserSerializer(
-            User.objects.get(username=single_user_data['username'])
-        )
+        response = self.client.post(reverse('users'), data=single_user_data)
+        created_user = User.objects.get(username=single_user_data['username'])
+        serializer = UserSerializer(created_user)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data, serializer.data)
         self.assertTrue(response.data != [])
+        self.assertTrue(created_user.cart.all().exists())
 
     def test_get(self):
         [User.objects.create(**user_data) for user_data in users_data_list]
