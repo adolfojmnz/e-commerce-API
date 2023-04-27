ca3109046a9897b07e79ccb50bae2c7f77b34e36
Adolfo Jimenez <adolfoj@protonmail.com>
2023-04-27 09:37:30 -0400

    Add tests for endpoints on `carts` app
    Tests HTTP Methods:
      - GET in `api/carts` endpoint
      - GET in `api/carts/{cartId}` endpoint
      - GET and POST in `api/cart-items` endpoint
      - GET, PATCH and DELETE in `api/cart-items/[cart-itemId]` endpoint

--- carts/tests/helpers.py
+++ carts/tests/helpers.py
@@ -5,17 +5,17 @@ from products.tests.helpers import create_product
 from carts.models import Cart, CartItem
 
 
-def create_cart():
+def create_cart(user=None):
     cart = Cart.objects.create(
-        user = UserTestMixin().create_customer(),
+        user = user or UserTestMixin().create_customer(),
     )
     cart.save()
     return cart
 
-def create_cart_item():
-    product = create_product()
+def create_cart_item(cart=None, product=None):
+    product = product or create_product()
     cart_item = CartItem.objects.create(
-        cart = create_cart(),
+        cart = cart or create_cart(),
         product = product,
         quantity = 10,
         sub_total = product.price * 10,
