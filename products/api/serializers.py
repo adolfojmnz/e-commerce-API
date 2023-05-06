fc3b436e5150290d270f5debe2066b45e29bc099
Adolfo Jimenez <adolfoj@protonmail.com>
2023-05-06 15:22:25 -0400

    ProductSerializer now serializes product rating and number of reviews

--- products/api/serializers.py
+++ products/api/serializers.py
@@ -1,3 +1,6 @@
+from django.db.models import Avg
+from django.urls import reverse
+
 from rest_framework import serializers
 
 from products.models import Product
@@ -17,6 +20,8 @@ class ProductSerializer(serializers.ModelSerializer):
     )
     available = serializers.SerializerMethodField()
     quantity = serializers.SerializerMethodField()
+    rating = serializers.SerializerMethodField()
+    total_reviews = serializers.SerializerMethodField()
 
     def get_available(self, product):
         return product.inventory.quantity > 0 and product.available
@@ -24,10 +29,19 @@ class ProductSerializer(serializers.ModelSerializer):
     def get_quantity(self, product):
         return product.inventory.quantity
 
+    def get_rating(self, product):
+        return product.reviews.aggregate(
+            rating=Avg('rating')
+        )['rating']
+
+    def get_total_reviews(self, product):
+        return product.reviews.count()
+
     class Meta:
         model = Product
         fields = [
             'id', 'name', 'brand', 'image', 'description', 'specifications',
-            'price', 'vendor', 'category', 'available', 'quantity',
+            'price', 'vendor', 'category', 'available', 'quantity', 'rating',
+            'total_reviews',
         ]
 
