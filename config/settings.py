af1d9c203aa06f0fb89cea3492cbff97982a039a
Adolfo Jimenez <adolfoj@protonmail.com>
2023-07-17 10:27:03 -0400

    Disable Refresh Token Rotation

--- config/settings.py
+++ config/settings.py
@@ -128,6 +128,6 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
      'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
      'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
-     'ROTATE_REFRESH_TOKENS': True,
+     'ROTATE_REFRESH_TOKENS': False,
      'BLACKLIST_AFTER_ROTATION': True
 }
