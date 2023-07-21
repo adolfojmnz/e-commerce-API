2e39283853f79eb60defd3ece12e3c68bac0247e
Adolfo Jimenez <adolfoj@protonmail.com>
2023-07-21 13:41:01 -0400

    Fix accidental datetime updates on `added_on` attribute ...
    Previously, the `added_on` attribute was being updated with
    every performed update, which was not intended.
    
    This commit fixes the issue by setting the `auto_now_add` option
    on the `added_on` attribute, which ensures that it is only set
    once on creation and remains unchanged on subsequent saves.

--- inventory/models.py
+++ inventory/models.py
@@ -6,7 +6,7 @@ class InventoryItem(models.Model):
                                 on_delete=models.PROTECT,
                                 related_name='inventory')
     quantity = models.IntegerField(default=0)
-    added_on = models.DateTimeField(auto_now=True)
+    added_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self) -> str:
