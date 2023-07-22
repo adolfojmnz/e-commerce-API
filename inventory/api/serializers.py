1ec877fa5182acc4095ec9c85bd03fecc99364b5
Adolfo Jimenez <adolfoj@protonmail.com>
2023-07-22 08:46:44 -0400

    Set min_value constraint for InventoryItem.quantity ...
    Set `min_value=0` constraint for quantity in InventoryItemSerializer.
    This will prevent setting the quantity of a product in stock to be
    less than zero.
    
    Additionally, set `product`, `updated_on`, and `added_on` attributes as
    read only.
    
    - The associated product of an inventory item instance shouldn't be changed.
    - The `updated_on` attribute is automatically update everytime the
      inventory item instance is updated.
    - The `added_on` attribute records the datetime of the instance creation,
      therefore, it should'n be changed.

--- inventory/api/serializers.py
+++ inventory/api/serializers.py
@@ -8,4 +8,9 @@ class InventoryItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = InventoryItem
         fields = '__all__'
-
+        extra_kwargs = {
+            'quantity': {'min_value': 0},
+            'product': {'read_only': True},
+            'updated_on': {'read_only': True},
+            'added_on': {'read_only': True},
+        }
