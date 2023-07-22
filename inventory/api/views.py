84b178a0135878b0590c8ef0aaa2b105cd2e6f3b
Adolfo Jimenez <adolfoj@protonmail.com>
2023-07-22 09:24:51 -0400

    Prevent updates in InventoryItem.updated_on for PATCH request ...
    Override the patch method on InventoryItemSingleView so no updates
    in InventoryItem.updated_on are performed unless an actual update
    on the InventoryItem instance occurs.
    
    Previously, the inventoryItem.updated_on attribute was being updated
    with PATCH requests that didn't even had a payload.
    
    The changes provided in this commit verify that the PATCH requests
    contain a valid non-empty payload and if so, the update is then performed.

--- inventory/api/views.py
+++ inventory/api/views.py
@@ -2,6 +2,8 @@ from rest_framework.generics import (
     ListAPIView, RetrieveUpdateAPIView
 )
 from rest_framework.permissions import IsAdminUser
+from rest_framework.response import Response
+from rest_framework import status
 
 from inventory.models import InventoryItem
 from inventory.api.serializers import InventoryItemSerializer
@@ -20,3 +22,12 @@ class InventoryItemSingleView(RetrieveUpdateAPIView):
     serializer_class = InventoryItemSerializer
     permission_classes = [IsAdminUser]
 
+    def patch(self, request, *args, **kwargs):
+        serializer = self.get_serializer(instance=self.get_object(),
+                                        data=request.data,
+                                        partial=True)
+        if serializer.is_valid():
+            if serializer.validated_data.get('quantity') is not None:
+                serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
